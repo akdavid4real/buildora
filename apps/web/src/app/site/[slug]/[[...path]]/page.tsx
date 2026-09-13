004e91ff1a9daf2e837d826774597e58b73ea508
Ak David <120152054+akdavid4real@users.noreply.github.com>
2026-09-13 10:50:34 +0100

    Route generated Products navigation to storefront

--- apps/web/src/app/site/[slug]/[[...path]]/page.tsx
+++ apps/web/src/app/site/[slug]/[[...path]]/page.tsx
@@ -3,6 +3,8 @@ import { Storefront } from '../../../../components/storefront';
 
 export default function PublicSitePage({ params }: { params: { slug: string; path?: string[] } }) {
   const path = params.path ?? [];
-  if (path[0] === 'shop') return <Storefront slug={params.slug} productSlug={path[1]} />;
+  if (path[0] === 'shop' || path[0] === 'products') {
+    return <Storefront slug={params.slug} productSlug={path[0] === 'shop' ? path[1] : undefined} />;
+  }
   return <PublicSite slug={params.slug} path={path} />;
 }
