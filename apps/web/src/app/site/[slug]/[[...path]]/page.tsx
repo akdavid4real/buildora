b1c63dfef75bb2fc949d6dc0762730e1bdd55edd
Ak David <120152054+akdavid4real@users.noreply.github.com>
2026-09-13 08:17:05 +0100

    Route shop paths to storefront

--- apps/web/src/app/site/[slug]/[[...path]]/page.tsx
+++ apps/web/src/app/site/[slug]/[[...path]]/page.tsx
@@ -1,5 +1,8 @@
 import { PublicSite } from '../../../../components/public-site';
+import { Storefront } from '../../../../components/storefront';
 
 export default function PublicSitePage({ params }: { params: { slug: string; path?: string[] } }) {
-  return <PublicSite slug={params.slug} path={params.path ?? []} />;
+  const path = params.path ?? [];
+  if (path[0] === 'shop') return <Storefront slug={params.slug} productSlug={path[1]} />;
+  return <PublicSite slug={params.slug} path={path} />;
 }
